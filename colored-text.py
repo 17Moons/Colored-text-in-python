4f877aedcbb73b616d14cb648daae581f26962f5
17Moons <92429890+17Moons@users.noreply.github.com>
2021-12-19 21:34:21 +0530

    Add files via upload

---
+++ colored-text.py
@@ -0,0 +1,9 @@
+from typing import ForwardRef
+from colorama import init, Fore, Back, Style
+init()
+
+print(Fore.RED + Back.BLACK +'To be yourself'+ Fore.BLUE+' is truly a revolutionary act'
++Fore.GREEN+' and i think more and more people should try it,'+Fore.MAGENTA+" because it's gotten to me a pretty cool life"+Style.RESET_ALL)
+
+
+
